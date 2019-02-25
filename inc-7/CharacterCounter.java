c10d731703908dba62f9ba95f4acc66475dd5869
PreethamBomma <41102108+PreethamBomma@users.noreply.github.com>
2019-02-25 14:04:24 +0530

    Resubmitting INC-7

--- inc-7/CharacterCounter.java
+++ inc-7/CharacterCounter.java
@@ -1,73 +1,77 @@
-/*Using TextFile and a Map<Character,Integer>,
-create a program that takes the file name as a command line argument and counts the occurrence
-of all the different characters. Save the results in a text file.
-
-*/
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.*;
-import java.util.*;
+import java.util.logging.Logger;
+import java.util.Map;
+import java.util.TreeMap;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintStream;
 
-public class CharacterCounter
-{
-  public static String fileContents;
-  public static Map<Character,Integer> countCharacters(String filename)
-  {
-    try
-      {
-         fileContents = new String(Files.readAllBytes(Paths.get(filename)));
-      }
-      catch(IOException e)
-      {
-        e.printStackTrace();
-      }
-  Map<Character,Integer> map = new TreeMap<Character,Integer>();
-  for(int i = 0; i < fileContents.length(); i++) {
-    Character character = fileContents.charAt(i);
-    if(Character.isLetter(character)) {
-      if((map.keySet().contains(character))) {
-        int counter=map.get(character);
-        map.put(character,++counter);
-      }
-      else {
-        map.put(character, 1);
-        }
+public class CharacterCounter {
+ public static String fileContents;
+ private static Logger logger = Logger.getLogger("CharacterCounter log");
+ static final String outputFilePath = "output.txt";
+ static Map < Character, Integer > map = new TreeMap < Character, Integer > ();
+
+ /**
+    Method to count occurrences of each character in a String
+
+    @param eachLine read each line of file
+
+    @return character occurrences as a Map
+
+ */
+ public static Map < Character, Integer > countCharacters(String lineOfFile) {
+  //System.out.println("In method  "+ lineOfFile);
+  fileContents = lineOfFile;
+  for (int i = 0; i < fileContents.length(); i++) {
+   Character character = fileContents.charAt(i);
+   if (Character.isLetterOrDigit(character)) {
+    if ((map.keySet().contains(character))) {
+     int counter = map.get(character);
+     map.put(character, ++counter);
+    } 
+    else {
+     map.put(character, 1);
     }
+   }
   }
   return map;
-}
+ }
 
-public static void main(String[] args) {
-  String output="";
-  try
-  {
-    if(args[0]==null)
-    {
-      System.out.println("Usage: java CharacterCounter <filename>");
-    }
-    else
-    {
-       output=countCharacters(args[0]).toString();
-      System.out.println(output);
-    }
-  }
-  catch(ArrayIndexOutOfBoundsException e)
-  {
-      e.printStackTrace();
-  }
-//  catch(IOException e)
-  //{
-    //e.printStackTrace();
-  //}
-  try (PrintStream out = new PrintStream(new FileOutputStream("output.txt"))) {
-    out.print(output);
-    out.flush();
-    out.close();
-}
-catch(FileNotFoundException e)
-{
+ public static void main(String[] args) {
 
-}
-}
+  String output = "";
+  BufferedReader fileReader;
+  try {
+   if (args[0] == null) {
+    logger.info("Usage: java CharacterCounter <filename>");
+   } 
+   else {
+    try {
+     fileReader = new BufferedReader(new FileReader(args[0].toString()));
+     String line = fileReader.readLine();
+     while (line != null) {
+      output = countCharacters(line).toString();
+      line = fileReader.readLine();
+     }
+    } 
+    catch (IOException e) {
+     logger.info("Cannot read file");
+    }
+   }
+  } 
+  catch (ArrayIndexOutOfBoundsException e) {
+   logger.info("File to be read from is not given" + e.getMessage());
+  }
+  try (PrintStream out = new PrintStream(outputFilePath)) {
+   out.append(output);
+   out.flush();
+  } 
+  catch (IOException e) {
+   logger.info("Cannot open output file " + e.getMessage());
+  }
+ }
 }
